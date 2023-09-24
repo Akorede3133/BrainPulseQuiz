24a9ee7da18f4e05a2fc5777dbda909b26c44207
akorede <emiolasaheed1@gmail.com>
2023-09-24 17:24:42 +0100

    Create funtion to fecth quiz questions

--- src/features/quiz/quizSlice.ts
+++ src/features/quiz/quizSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { InitialStateProp } from "../../types";
 const initialState: InitialStateProp = {
   questions: [],
@@ -6,6 +6,15 @@ const initialState: InitialStateProp = {
   error: '',
 };
 
+export const getQuestions = createAsyncThunk('quiz/getQuestions', async () => {
+  try {
+    const response = await fetch('https://opentdb.com/api.php?amount=10');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+})
 
 export const quizeSlice = createSlice({
   name: 'quiz',
