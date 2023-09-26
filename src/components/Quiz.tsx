23c442a157dc93c0eb95ce4e9feae60d5da0d275
akorede <emiolasaheed1@gmail.com>
2023-09-26 18:11:16 +0100

    Indicate the chosen option by styling its respective button

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -15,14 +15,14 @@ const Quiz = () => {
       <ul className=" text-white px-4 min-h-[90vh] grid place-content-start gap-4">
         {
           questions.map((quest) => {
-            const {question, options, id } = quest;
+            const {question, options, id, selected } = quest;
             return (
               <li key={id} className=" border-b border-b-pink-700 pb-5">
                 <p className=" pb-4 text-2xl">{decodeHtmlEntities(question)}</p>
                 <ul className=" flex flex-wrap gap-4">
                   {
-                    options.map((option, index) => <li className="" key={index} onClick={()=> dispatch(selectAnswer({id, value: decodeHtmlEntities(option)}))}>
-                      <button className="text-sm cursor-pointer hover:bg-pink-600 border hover:border-0 border-pink-700 rounded-md px-4 py-1">{decodeHtmlEntities(option)}</button>
+                    options.map((option, index) => <li key={index} onClick={()=> dispatch(selectAnswer({id, value: decodeHtmlEntities(option)}))}>
+                      <button className={`${selected === option && 'bg-pink-500'} text-sm cursor-pointer hover:bg-pink-600 border hover:border-0 border-pink-700 rounded-md px-4 py-1`}>{decodeHtmlEntities(option)}</button>
                       </li>)
                   }
                 </ul>
